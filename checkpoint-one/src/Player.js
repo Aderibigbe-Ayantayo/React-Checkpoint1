b4273b7eb393ba2247cf2af14d77ab0e5540eabb
Fadeelson <adex_fadeel@ymail.com>
2024-08-26 13:29:50 -0700

    first commit

---
+++ checkpoint-one/src/Player.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img className="img" variant="top" src={imageUrl} alt={name} />
+      <Card.Body>
+        <Card.Title className="title">{name}</Card.Title>
+        <Card.Text>Team: {team}</Card.Text>
+        <Card.Text>Nationality: {nationality}</Card.Text>
+        <Card.Text>Jersey Number: {jerseyNumber}</Card.Text>
+        <Card.Text>Age: {age}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Player.defaultProps = {
+  name: "Unknown Player",
+  team: "Unknown Team",
+  nationality: "Unknown",
+  jerseyNumber: 0,
+  age: 0,
+  imageUrl: "https://via.placeholder.com/150",
+};
+
+export default Player;
---
+++ checkpoint-one/src/players.js
@@ -0,0 +1,30 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentine",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://creatufut.com/wp-content/uploads/2023/11/MESSI-FCB_FC24-400x617.png", // Replace with actual image URL
+  },
+  {
+    name: "Kylian Mbappé",
+    position: "Forward",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 10,
+    age: 24,
+    imageUrl: "https://creatufut.com/wp-content/uploads/2023/09/MBAPPE_PSG-400x646.png"
+  },
+  {
+    name: "Erling Haaland",
+    position: "Forward",
+    team: "Manchester City",
+    nationality: "Norwegian",
+    jerseyNumber: 10,
+    age: 23,
+    imageUrl: "https://game-assets.fut.gg/2024/cards/futgg-cards/84125165.webp"
+  }
+];
+
+export default players;
